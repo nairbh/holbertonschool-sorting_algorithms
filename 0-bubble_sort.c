a08a955aea143730996a0f27fda8f97a39589a9b
bricorne <bricepoitevineau@gmail.com>
2023-05-23 04:44:57 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,9 +2,10 @@
 
 void bubble_sort(int *array, size_t size) 
 {
-	for (size_t i = 0; i < size - 1; ++i)
+	size_t i, j;
+	for (i = 0; i < size - 1; ++i)
 	{
-		for (size_t j = 0; j < size - i - 1; ++j)
+		for (j = 0; j < size - i - 1; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{
