cb03fc1aad486355dcb2d20ce03eb9641de83df7
bricorne <bricepoitevineau@gmail.com>
2023-05-23 04:51:50 -0700

    betty task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,19 @@
 #include "sort.h"
 
-void bubble_sort(int *array, size_t size) 
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ *
+ *@array: array of numbers
+ *@size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+
+	if (!array || !size)
+		return;
+
 	for (i = 0; i < size - 1; ++i)
 	{
 		for (j = 0; j < size - i - 1; ++j)
@@ -10,6 +21,7 @@ void bubble_sort(int *array, size_t size)
 			if (array[j] > array[j + 1])
 			{
 				int temp = array[j];
+
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
