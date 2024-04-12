0c4c803dbede0084e116ff08f3adb7d584caf249
Houcine <113390396+nairbh@users.noreply.github.com>
2024-04-12 15:13:44 +0200

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,81 +1,88 @@
 #include "sort.h"
 
+void rec_quicksort(int *array, int low, int high, size_t size);
+int lomutoPartition(int *array, int low, int high, size_t size);
+
 /**
- * swap - swaps the values of two integers
- *
- * @a: first integer to be swapped
- * @b: second integer to be swapped
- */
-
-void swap(int *a, int *b)
+* swap - helper function that swaps the values of two given integers
+* @a : first int
+* @b : second int
+* Return: void
+*/
+void swap(int *a, int *b, int *array, size_t size)
 {
-	int temp = *a;
+        int temp;
 
-	*a = *b;
-	*b = temp;
+        temp = *a;
+        *a = *b;
+        *b = temp;
+        print_array(array, size);
 }
 
 /**
- * partition - sections an array using quick_sort algo
- * with Lomuto scheme
- *
- * @array: array of integers
- * @low: lowest index of the array
- * @high: highest index of the array
- * @size: size of the array
- * Return: position of pivot in array
- */
-
-int partition(int *array, int low, int high, size_t size)
+* rec_quicksort - recursively sorts a subarray of ints with quicksort algorithm
+* @array: pointer to the array to be sorted
+* @size: size of the array
+* @low: index of the start of the sub-array to be sorted
+* @high: index of the end of the sub-array to be sorted
+* Return: void
+*/
+void rec_quicksort(int *array, int low, int high, size_t size)
 {
-	int pivot_value = array[high];
-	int i = low - 1, j;
+        int parti;
 
-	for (j = low; j <= high; j++)
-	{
-		if (array[j] <= pivot_value)
-		{
-			i++;
-			if (i != j)
-			{
-				swap(&array[i], &array[j]);
-				print_array(array, size);
-			}
-		}
-	}
-	return (i);
+        if (low < high)
+        {
+                parti = lomutoPartition(array, low, high, size);
+                rec_quicksort(array, low, parti - 1, size);
+                rec_quicksort(array, parti + 1, high, size);
+        }
 }
 
 /**
- * quick_sort_recursive - recursive quick sort function
- *
- * @array: array to be sorted
- * @low: starting index of the subarray
- * @high: ending index of the subarray
- * @size: size of the array
- */
-
-void quick_sort_recursive(int *array, int low, int high, size_t size)
+* lomutoPartition - function that applies the Lomuto partition scheme
+*
+* Description: last element of subarray = pivot, then :
+*                               elements < pivot get to its left
+*                               elements > pivot get to its right.
+*
+* @array: pointer to the sub-array to be sorted
+* @low: index of the start of the sub-array
+* @high: index of the end of the sub-array
+* @size: size of the sub-array
+* Return: index of the pivot after partitioning
+*/
+int lomutoPartition(int *array, int low, int high, size_t size)
 {
-	if (low < high)
-	{
-		int pivot = partition(array, low, high, size);
+        int pivot = array[high];
+        int i = low - 1;
+        int j;
 
-		quick_sort_recursive(array, low, pivot - 1, size);
-		quick_sort_recursive(array, pivot + 1, high, size);
-	}
+        for (j = low ; j < high ; j++)
+        {
+                if (array[j] < pivot)
+                {
+                        i++;
+                        swap(&array[i], &array[j], array, size);
+                }
+        }
+        swap(&array[i + 1], &array[high], array, size);
+        return (i + 1);
 }
-/**
- * quick_sort - sorts the array in ascending order
- * using the quicksort algorithm
- *
- * @array: array to be sorted
- * @size: size of the array
- */
 
+/**
+ * quick_sort - Sorts an array of integers in ascending order using the
+ *              quicksort algorithm with Lomuto partition scheme.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ */
 void quick_sort(int *array, size_t size)
 {
-	if (!array)
-		return;
-	quick_sort_recursive(array, 0, size - 1, size);
+        int low = 0;
+        int high = size - 1;
+
+        if (size < 2)
+                return;
+
+        rec_quicksort(array, low, high, size);
 }
